00cdf75b21bb636ece2c126d84692c30c9b14dbb
devilAtlastBreath <omkarp.0906@gmail.com>
2025-03-15 08:53:51 +0000

    main

--- 02assignClg-Prac/02assignPrg2.c
+++ 02assignClg-Prac/02assignPrg2.c
@@ -1,6 +1,29 @@
-/*WAP to accept the number and check that Prime or Not*/
+/*WAP to accept the number and check that Prime or Not in separate function*/
 #include <stdio.h>
+int primeFunc(int *ptr){
+    int flag = 0;
+    for(int i = 2 ; i * i <= *ptr; i++){
+        if(*ptr % i == 0){
+            flag = 1;
+        }
+    }
+    if (flag == 0)
+        return 1;
+    else
+        return 0;
+}
 int main(){
-    
+    int num;
+    int *ptr = &num;
+
+    printf("Enter the number to check whether Prime or not : ");
+    scanf("%d", ptr);
+
+    int res = primeFunc(ptr);
+
+    if (res == 1)
+        printf("The number %d is Prime",*ptr);
+    else
+        printf("The number %d is Not Prime",*ptr);
     return 0;
 }
