69c7076a935221ca35c465e6f46c7d506cdfb48a
devilAtlastBreath <omkarp.0906@gmail.com>
2025-03-15 06:54:33 +0000

    main

--- 01assignClg-Prac/01assignPrg1.c
+++ 01assignClg-Prac/01assignPrg1.c
@@ -2,11 +2,14 @@
 int main(){
     int num;
     int *ptr = &num;
+
     printf("Enter the number to check whether it is Even or Odd : ");
     scanf("%d",ptr);
+
     if(*ptr % 2 == 0)
         printf("The given number %d is even",*ptr);
     else
         printf("The Entered number %d is odd",*ptr);
+        
     return 0;
 }
---
+++ 01assignClg-Prac/01assignPrg2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    int *ptr1 = &num1; 
+    int *ptr2 = &num2;  
+
+    printf("Enter the number 1 to check maximum number of it: ");
+    scanf("%d", ptr1);
+    printf("Enter the number 2 to check maximum number of it: ");
+    scanf("%d", ptr2);
+
+    if (*ptr1 > *ptr2) {
+        printf("The number %d is greater than number %d\n", *ptr1, *ptr2);
+    } else if (*ptr1 < *ptr2) {
+        printf("The number %d is greater than number %d\n", *ptr2, *ptr1);
+    } else {
+        printf("Both numbers are equal\n");
+    }
+
+    return 0;
+}
