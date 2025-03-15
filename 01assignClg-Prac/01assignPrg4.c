b3e8c7fcd3d0dabe1ae4f70324c26949dc8cb7d2
devilAtlastBreath <omkarp.0906@gmail.com>
2025-03-15 07:25:56 +0000

    main

--- 01assignClg-Prac/01assignPrg4.c
+++ 01assignClg-Prac/01assignPrg4.c
@@ -5,6 +5,15 @@ int main(){
     int *ptr1 = &num1;
     int *ptr2 = &num2;
 
-    pr
+    printf("Enter the number 1 to interchange value : ");
+    scanf("%d",ptr1);
+    printf("Enter the number 2 to interchange value : ");
+    scanf("%d",ptr2);
+
+    int temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+
+    printf("Entered number are interchanged %d is num1 and %d is num2",*ptr1,*ptr2);
     return 0;
 }
