0471c5002b125979b9ddf0f873180b1c0c450662
devilAtlastBreath <omkarp.0906@gmail.com>
2025-03-15 07:19:57 +0000

    main

---
+++ 01assignClg-Prac/01assignPrg4.c
@@ -0,0 +1,10 @@
+/*WAP to interchange value of two variables using pointer*/
+#include <stdio.h>
+int main(){
+    int num1,num2;
+    int *ptr1 = &num1;
+    int *ptr2 = &num2;
+
+    pr
+    return 0;
+}
